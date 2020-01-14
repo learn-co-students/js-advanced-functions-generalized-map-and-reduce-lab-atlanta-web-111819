2d1c30959648dfb80d85861802cb336e0a72609e
RobertTTaylor9517 <robt15@hotmail.com>
2020-01-14 17:02:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, func){
+    let r =[]
+    for(let i=0;i<array.length;i++){
+        r.push(func(array[i]))
+    }
+    return r
+} 
+
+function reduce(array, func, num){
+    let val
+    let i
+    if(num === undefined){
+        val = array[0]
+        i = 1
+    }else{
+        val = num
+        i = 0
+    }
+
+    for(;i<array.length;i++){
+        val = func(array[i], val)
+    }
+    return val
+}
